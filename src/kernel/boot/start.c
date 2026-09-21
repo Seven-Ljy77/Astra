36d89ff0c202486a473551fb1e16a39dc3af8fc9
Ljy <liujiayi200701@163.com>
2026-09-21 19:29:18 +0800

    add note to src/kernel/boot/start.c

--- src/kernel/boot/start.c
+++ src/kernel/boot/start.c
@@ -16,13 +16,13 @@ void start()
     w_tp(id);
 
     // 修改mstatus寄存器，假装上一个状态是S-mode
-    uint64 status = r_mstatus();
-    status &= ~MSTATUS_MPP_MASK;
-    status |= MSTATUS_MPP_S;
-    w_mstatus(status);
+    uint64 status = r_mstatus();   // mstatus 寄存器有一个 MPP 字段为第 11 , 12 位置 bit , 用来记录切换的下一个状态
+    status &= ~MSTATUS_MPP_MASK;   // 先把 11 , 12 两个 bit 清零
+    status |= MSTATUS_MPP_S;       // 再把这两个 bit 的值设置为 S-Mode 应有的值
+    w_mstatus(status);             // 将新值写入寄存器
 
     // 设置M-mode的返回地址
-
+    
     // 触发状态迁移，回到上一个状态（M-mode->S-mode）
 
 }
