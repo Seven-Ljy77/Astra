cbc486644ee08fd4aff62bf2a482ccd45be4b796
Ljy <liujiayi200701@163.com>
2026-09-21 19:57:12 +0800

    finish start.c

--- src/kernel/boot/start.c
+++ src/kernel/boot/start.c
@@ -22,7 +22,7 @@ void start()
     w_mstatus(status);             // 将新值写入寄存器
 
     // 设置M-mode的返回地址
-    
+    w_mepc((uint64)main);
     // 触发状态迁移，回到上一个状态（M-mode->S-mode）
-
+    asm volatile("mret");
 }
--- src/kernel/main.c
+++ src/kernel/main.c
@@ -3,5 +3,7 @@
 
 int main()
 {
+    while (1){}
     
+    return 0;
 }
