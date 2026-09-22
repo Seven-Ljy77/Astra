388b7391ec3a3e3452b8e0501c9fa12eea493957
Ljy <liujiayi200701@163.com>
2026-09-22 15:46:18 +0800

    finish main.c , add started tag

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,13 +1,22 @@
 #include "arch/mod.h"
 #include "lib/mod.h"
 
+volatile static int started = 0;
+
 int main()
 {
-    print_init();
+    if (r_tp() == 0){ // init
+        print_init();
+        printf("\n");
+        printf("kernel is booting !\n\n");
+        started = 1;
+    }
+
+    while (started == 0) {}
 
     printf("CPU %d is booting !\n" , r_tp());
 
-    while (1){}
+    while (1) {}
 
     return 0;
 }
